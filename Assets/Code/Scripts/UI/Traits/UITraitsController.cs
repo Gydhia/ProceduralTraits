7a38f7a16812717870d29847c44561917e07ac39
Gydhia <60605038+Gydhia@users.noreply.github.com>
2024-02-07 12:36:03 +0100

    Added base dropdown to add trait

--- Assets/Code/Scripts/UI/Traits/UITraitsController.cs
+++ Assets/Code/Scripts/UI/Traits/UITraitsController.cs
@@ -1,11 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class UITraitsController : MonoBehaviour
 {
+    [SerializeField] private TMP_Dropdown m_AddTrait;
+    
     public void Init()
     {
-        
+        m_AddTrait.onValueChanged.AddListener(OnDropDownSelection);
+    }
+
+    public void OnDropDownSelection(int index)
+    {
+        Debug.Log("Trait to add : " + index);
     }
 }
